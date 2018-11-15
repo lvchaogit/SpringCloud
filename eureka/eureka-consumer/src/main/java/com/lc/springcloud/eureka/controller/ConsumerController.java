a1c2ea9f179a8fb55816cb454880a427b62f5007
252949240@qq.com <252949240@qq.com>
2018-11-16 01:02:19 +0800

    新增eureka-consumer

---
+++ eureka/eureka-consumer/src/main/java/com/lc/springcloud/eureka/controller/ConsumerController.java
@@ -0,0 +1,26 @@
+package com.lc.springcloud.eureka.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Eureka客户端Controller
+ * @author LC
+ * @date 2018/11/9
+ */
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add() {
+        System.out.println(restTemplate.getForEntity("http://EUREKA-PROVIDER/add?a=10&b=20", String.class).getBody());
+        return restTemplate.getForEntity("http://EUREKA-PROVIDER/add?a=10&b=20", String.class).getBody();
+    }
+}
+ 
