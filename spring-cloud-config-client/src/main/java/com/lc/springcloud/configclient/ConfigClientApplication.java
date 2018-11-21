1324c897b8b075145f62f6ee534b5737501506ac
252949240@qq.com <252949240@qq.com>
2018-11-21 23:40:51 +0800

    集成eureka 通过访问config server获取配置

--- spring-cloud-config-client/src/main/java/com/lc/springcloud/configclient/ConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/lc/springcloud/configclient/ConfigClientApplication.java
@@ -3,10 +3,11 @@ package com.lc.springcloud.configclient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 @MapperScan(basePackages = {"com.lc.springcloud.configclient.mapper"})
 public class ConfigClientApplication {
 
